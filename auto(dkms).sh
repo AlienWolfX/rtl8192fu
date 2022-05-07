d0e757a17fba29a56beb720cec5e3f9c14e85f2e
AlienWolfX <cruizallen2@gmail.com>
2022-05-08 00:01:57 +0800

    AAAA

--- auto(dkms).sh
+++ auto(dkms).sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#!Tested on Kali only but may work on other debian based distro
+#!Working on Ubuntu Impish
 echo "RTL8192FU auto-installer script via dkms"
 echo "Created by AlienWolfX"
-sleep 2
 echo -n "Install or Remove [I/R]: " 
 read op
 if [[ $op == I || $op == i ]]; then
@@ -10,12 +9,12 @@ if [[ $op == I || $op == i ]]; then
 		sudo apt-get update
 		sudo apt-get autoremove
 		sudo apt-get install -y linux-headers-$(uname -r) 
-		sudo apt-get install make gcc bc -y
-		sudo apt-get install build-essential dkms -y 
+		sudo apt-get install make gcc bc build-essential dkms -y 
 		sudo chmod a+x ./dkms-* 
 		sudo ./dkms-install.sh
-		sudo modprobe 8192fu
 		sleep 3
+		sudo modprobe 8192fu
+		sleep 2
 		echo "Installation Done Exiting..."
 		sleep 2
 		clear
@@ -25,6 +24,8 @@ elif [[ $op == R || $op == r ]]; then
  		sudo ./dkms-remove.sh
 else
 	echo "Not an option dummy :) Exiting..."
+	sleep 2
 	clear
 	exit
 fi
+clear
